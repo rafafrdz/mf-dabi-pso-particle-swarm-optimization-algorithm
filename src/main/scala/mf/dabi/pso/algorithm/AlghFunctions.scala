c51f206eaacb42a0fa08f203c00a2f4dc7f613dd
Rafael Fernández <40717893+rafafrdz@users.noreply.github.com>
2021-10-04 08:51:54 +0200

    add more fitness function

--- src/main/scala/mf/dabi/pso/algorithm/AlghFunctions.scala
+++ src/main/scala/mf/dabi/pso/algorithm/AlghFunctions.scala
@@ -4,4 +4,15 @@ import mf.dabi.pso.algorithm.space.SearchSpace.Particle
 
 object AlghFunctions {
   val sphere: Particle => Double = (x: Particle) => x.toArray.map(d => math.pow(d, 2)).sum
+
+  private def rosenbrockF(xs: Array[Double]): Int => Double = (i: Int) => 100 * math.pow(xs(i + 1) - math.pow(xs(i), 2), 2) + math.pow(xs(i) - 1, 2)
+
+  val rosenbrock: Particle => Double = (x: Particle) => {
+    val values: Array[Double] = x.toArray
+    (0 until (values.length - 1)).map(rosenbrockF(values)).sum
+  }
+
+  private def rastriginF: Double => Double = (x: Double) => math.pow(x, 2) - 10 * math.cos(2 * math.Pi * x) + 10
+
+  val rastrigin: Particle => Double = (x: Particle) => x.toArray.map(rastriginF).sum
 }
